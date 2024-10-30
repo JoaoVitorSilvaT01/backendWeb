defff75aad259c18fc98f4898b8787374270ef19
unknown <vitorjoao201412@unifei.edu.br>
2024-10-30 18:08:12 -0300

    commit atualizado

--- index.js
+++ index.js
@@ -58,6 +58,77 @@ app.get("/usuarios", (req, res) => {
     }
   });
 
+  app.delete("/usuarios/:id", (req, res) => {
+    try {
+      console.log("Rota: delete/" + req.params.id);
+      client.query(
+        "DELETE FROM Usuarios WHERE id = $1",
+        [req.params.id],
+        (err, result) => {
+          if (err) {
+            return console.error("Erro ao executar a qry de DELETE", err);
+          } else {
+            if (result.rowCount == 0) {
+              res.status(404).json({ info: "Registro não encontrado." });
+            } else {
+              res
+                .status(200)
+                .json({ info: `Registro excluído. Código: ${req.params.id}` });
+            }
+          }
+          console.log(result);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  app.post("/usuarios", (req, res) => {
+    try {
+      console.log("Alguém enviou um post com os dados:", req.body);
+      const { nome, email, altura, peso } = req.body;
+      client.query(
+        "INSERT INTO Usuarios (nome, email, altura, peso) VALUES ($1, $2, $3, $4) RETURNING * ",
+        [nome, email, altura, peso],
+        (err, result) => {
+          if (err) {
+            return console.error("Erro ao executar a qry de INSERT", err);
+          }
+          const { id } = result.rows[0];
+          res.setHeader("id", `${id}`);
+          res.status(201).json(result.rows[0]);
+          console.log(result);
+        }
+      );
+    } catch (erro) {
+      console.error(erro);
+    }
+  });
+
+  app.put("/usuarios/:id", (req, res) => {
+    try {
+      console.log("Alguém enviou um update com os dados:", req.body);
+      const id = req.params.id;
+      const { nome, email, altura, peso } = req.body;
+      client.query(
+        "UPDATE Usuarios SET nome=$1, email=$2, altura=$3, peso=$4 WHERE id =$5 ",
+        [nome, email, altura, peso, id],
+        (err, result) => {
+          if (err) {
+            return console.error("Erro ao executar a qry de UPDATE", err);
+          } else {
+            res.setHeader("id", id);
+            res.status(202).json({ identificador: id });
+            console.log(result);
+          }
+        }
+      );
+    } catch (erro) {
+      console.error(erro);
+    }
+  });
+
 app.get("/", (req, res) => {
   console.log("Response ok.");
   res.send("Ok – Servidor disponível.");
@@ -66,3 +137,4 @@ app.listen(config.port, () =>
   console.log("Servidor funcionando na porta " + config.port)
 );
 
+module.exports = app;
